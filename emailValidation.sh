26c3eba1fe5d718d41f7cceba878473b7e449e30
NishaRaut <nisharaut84@gmail.com>
2020-01-28 17:17:18 +0530

    Added: .co,.com etc accepted

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 read -p "Enter the string: " string
-pattern="^[a-zA-Z]+@[bridgelabz]"
+pattern="^[a-zA-Z]+@[bridgelabz]+[.]{1}[a-zA-Z]{2,3}$"
 if [[ $string =~ $pattern ]]
 then
 	echo "VALID"
